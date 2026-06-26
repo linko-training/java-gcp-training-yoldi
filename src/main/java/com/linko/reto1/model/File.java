2c8a890a3d2349bf608d02ab6ea1cede4053c8c9
yoldi <yoldirgb@protonmail.com>
2026-06-26 12:09:23 -0600

    refactor: usar name como identificador del CRUD y eliminar campo id

--- src/main/java/com/linko/reto1/model/File.java
+++ src/main/java/com/linko/reto1/model/File.java
@@ -2,24 +2,14 @@ package com.linko.reto1.model;
 
 public class File {
 
-    private String id;
     private String name;
     private long size;
     private String url;
 
-    public File(String id, String name, long size, String url) {
-        this.id = id;
+    public File(String name, long size, String url) {
         this.name = name;
         this.size = size;
         this.url = url;
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
     }
 
     public String getName() {
